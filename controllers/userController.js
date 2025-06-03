30db45743b941861ce11a0a68d1cdd7e5abe53d9
modit1802 <21803019@mail.jiit.ac.in>
2025-06-03 20:00:04 +0530

    changes in success url

--- controllers/userController.js
+++ controllers/userController.js
@@ -342,7 +342,7 @@ const createPaymentIntent = async (req, res) => {
                 quantity: 1,
             }],
             mode: 'payment',
-            success_url:`${process.env.FRONTEND_URL}/my-appointments?session_id={CHECKOUT_SESSION_ID}`,
+            success_url: 'https://health-line-frontend-rhc2.vercel.app/my-appointments?session_id={CHECKOUT_SESSION_ID}',
             cancel_url: `${process.env.FRONTEND_URL}/my-appointments`,
         });
 
